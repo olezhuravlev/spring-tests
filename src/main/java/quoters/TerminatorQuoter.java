d37288a7e7a297f213a521841c82f0fa3f59fa03
Oleg Zhuravlev <olezhuravlev@gmail.com>
2023-02-08 16:36:58 +0100

    Method init() implemented

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -1,11 +1,22 @@
 package quoters;
 
+import javax.annotation.PostConstruct;
+
 public class TerminatorQuoter implements Quoter {
 
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
 
     private String message;
+
+    public TerminatorQuoter() {
+        System.out.println("Phase 1: " + repeat);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Phase 2: " + repeat);
+    }
 
     public void setMessage(String message) {
         this.message = message;
--- src/main/java/ru/otus/spring/Main.java
+++ src/main/java/ru/otus/spring/Main.java
@@ -4,8 +4,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import quoters.TerminatorQuoter;
 
 public class Main {
-    // 1. BeanDefinitionBuilder - scans context.
-    // 2. BeanFactory - creates instances.
+    // 1. BeanDefinitionDocumentReader - parses context for bean definitions;
+    // 2. BeanFactory:
+    //     - retrieves BeanPostProcessor-s from bean definitions for further usage;
+    //     - creates bean according to the configuration (first phase);
+    //     - calls 'init()' method of the bean;
+    //     - processes bean with BeanPostProcessor (second phase);
     // 3. BeanPostProcessor - processes beans before publication.
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:context.xml");
