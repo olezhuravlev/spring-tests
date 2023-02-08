66e0e324df52b669b5f254108978c06f09a93019
Oleg Zhuravlev <olezhuravlev@gmail.com>
2023-02-08 16:08:41 +0100

    Custom annotation implemented

--- src/main/java/ru/otus/spring/Main.java
+++ src/main/java/ru/otus/spring/Main.java
@@ -1,7 +1,15 @@
 package ru.otus.spring;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import quoters.TerminatorQuoter;
+
 public class Main {
+    // 1. BeanDefinitionBuilder - scans context.
+    // 2. BeanFactory - creates instances.
+    // 3. BeanPostProcessor - processes beans before publication.
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:context.xml");
+        context.getBean(TerminatorQuoter.class).sayQuote();
     }
 }
+
