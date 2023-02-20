0055c00e1a4a470d59b358325f6681ee5de34974
Oleg Zhuravlev <olezhuravlev@gmail.com>
2023-02-20 14:54:34 +0100

    Custom scope implemented

---
+++ src/main/java/screensaver/PeriodicalScopeConfigurer.java
@@ -0,0 +1,68 @@
+package screensaver;
+
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.config.Scope;
+
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.time.LocalTime.now;
+
+public class PeriodicalScopeConfigurer implements Scope {
+
+    private Map<String, Pair<LocalTime, Object>> map = new HashMap<>();
+
+    private class Pair<K, V> {
+
+        K key;
+        V value;
+
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+
+    @Override
+    public Object get(String name, ObjectFactory<?> objectFactory) {
+        if (map.containsKey(name)) {
+            Pair<LocalTime, Object> pair = map.get(name);
+            int secondsSinceLastRequest = now().getSecond() - pair.getKey().getSecond();
+            if (secondsSinceLastRequest > 3) {
+                map.put(name, new Pair<>(now(), objectFactory.getObject()));
+            }
+        } else {
+            map.put(name, new Pair<>(now(), objectFactory.getObject()));
+        }
+
+        return map.get(name).getValue();
+    }
+
+    @Override
+    public Object remove(String name) {
+        return null;
+    }
+
+    @Override
+    public void registerDestructionCallback(String name, Runnable callback) {
+    }
+
+    @Override
+    public Object resolveContextualObject(String key) {
+        return null;
+    }
+
+    @Override
+    public String getConversationId() {
+        return null;
+    }
+}
